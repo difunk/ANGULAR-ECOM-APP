0b2d785d44199b9db20fcb0082b90e30cbd4c957
difunk <so0mewhat2@gmail.com>
2025-05-21 14:22:35 +0200

    add: create & integrate order summary component into cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Product } from '../../models/products.model';
 import { CartItemComponent } from "./cart-item/cart-item.component";
+import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItemComponent],
+  imports: [CartItemComponent, OrderSummaryComponent],
   template: `
   <div class="p-6 flex flex-col gap-4">
     <h2 class="text-2xl">Shopping Cart</h2>
@@ -13,6 +14,7 @@ import { CartItemComponent } from "./cart-item/cart-item.component";
       @for (cartItem of cartService.cart(); track cartItem) {
         <app-cart-item [item]="cartItem"/>
       }
+      <app-order-summary />
     } @else {
       <p>Dein Warenkorb ist leer.</p>
     }
